6fa91a912f0b01db4a63459557d4e49b9985e444
Jan Michek <jan@janmichek.cz>
2025-11-10 15:25:21 +0100

    remove unused vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -10,14 +10,6 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_APP_ID: string
   readonly VITE_FIREBASE_MEASUREMENT_ID: string
   readonly VITE_FIREBASE_DATABASE_URL?: string
-
-  // Leaderboard Firebase App
-  readonly VITE_LEADERBOARD_API_KEY: string
-  readonly VITE_LEADERBOARD_AUTH_DOMAIN: string
-  readonly VITE_LEADERBOARD_PROJECT_ID: string
-  readonly VITE_LEADERBOARD_STORAGE_BUCKET: string
-  readonly VITE_LEADERBOARD_MESSAGING_SENDER_ID: string
-  readonly VITE_LEADERBOARD_APP_ID: string
 }
 
 interface ImportMeta {
